dfb924bef86e56f6ac17be897355c8c6d2bf0e51
Adrian Aisemberg <adrianai@microsoft.com>
2018-08-30 15:47:03 -0700

    brew install mono-libgdiplus

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+brew install mono-libgdiplus
 ANDROID_PROJECT_PATH=./
 echo patching icon with shape: $badge_shape, text: $badge_text, color: $badge_color
 dotnet ./ext/PatchIcon.dll -p=$ANDROID_PROJECT_PATH -s=$badge_shape -t=$badge_text -c=$badge_color
