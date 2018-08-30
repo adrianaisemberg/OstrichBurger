93b989f4418094b0d38830a0d7a37c67abee2b27
Adrian Aisemberg <adrianai@microsoft.com>
2018-08-30 15:19:59 -0700

    call the patcher

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-dotnet ./ext/PatchIcon.dll -p=path -s=shape -t=text -c=color
+ANDROID_PROJECT_PATH=./
+echo patching icon with shape: $badge_shape, text: $badge_text, color: $badge_color
+dotnet ./ext/PatchIcon.dll -p=$ANDROID_PROJECT_PATH -s=$badge_shape -t=$badge_text -c=$badge_color
