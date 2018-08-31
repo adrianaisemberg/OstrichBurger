349c998764c85ca3a9f82bf8517653c212f97313
Adrian Aisemberg <adrianai@microsoft.com>
2018-08-31 16:01:35 -0700

    patcher

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -4,8 +4,8 @@ then
   echo not patching any icon this time
 else
   brew install mono-libgdiplus
-  ANDROID_PROJECT_PATH=./
+  PROJECT_PATH=./
   echo patching icon with shape: $badge_shape, text: $badge_text, color: $badge_color
-  dotnet ./ext/PatchIcon.dll android -projectPath=$ANDROID_PROJECT_PATH -shape=$badge_shape -text=$badge_text -colorHex=$badge_color -appIdSuffix=$app_id_suffix
+  dotnet ./ext/PatchIcon.dll android -projectPath=$PROJECT_PATH -shape=$badge_shape -text=$badge_text -colorHex=$badge_color -appIdSuffix=$app_id_suffix
 fi
 
